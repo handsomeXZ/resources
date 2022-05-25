c787b089e29b4afc44b3e898a7d458fb5cb3aff7
jtj <9900647+vcafilm@user.noreply.gitee.com>
2022-05-25 18:03:16 +0800

    更新了上传页面

---
+++ js/uploader.js
@@ -0,0 +1,6 @@
+document.getElementById("uploaderSize").addEventListener("click",function(){
+	var uploader = document.querySelector(".uploaderbackground").style.width="4.7rem";
+	var uploader = document.querySelector(".uploaderEmpty").style.width="4.7rem";
+	console.log(uploader);
+	
+})
